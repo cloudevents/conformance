1a21e5b7808c01135994ea613cca451b169738ec
Scott Nichols <nicholss@google.com>
2019-08-26 08:06:37 -0700

    unit test now deps on no external files.
    Signed-off-by: Scott Nichols <nicholss@google.com>

--- pkg/http/http_yaml_test.go
+++ pkg/http/http_yaml_test.go
@@ -1,17 +1,45 @@
 package http
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
+	"os"
 	"testing"
 
 	"github.com/cloudevents/conformance/pkg/event"
 )
 
+var helloWorldV03 = `
+ContextAttributes:
+  specversion: 0.3
+  type: io.cloudevents.unittest
+  id: unit-test-0001
+  source: //github.com/cloudevents/conformance/pkg/http/unittest
+  datacontenttype: application/json; charset=utf-8
+Data: |
+  {"msg":"Hello, World!"}
+`
+
+func makeUnitTestYaml() (string, func()) {
+	file, err := ioutil.TempFile("", "unit_test_*.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, _ = file.WriteString(helloWorldV03)
+	return file.Name(), func() {
+		_ = os.Remove(file.Name())
+	}
+}
+
 func TestHTTP_Binary_v3_yaml(t *testing.T) {
+	tmp, done := makeUnitTestYaml()
+	defer done()
+
 	server := httptest.NewServer(&dump{t: t})
-	events := event.FromYaml(event.File("v0.3"), true)
+	events := event.FromYaml(tmp, true)
 	testServer := server.URL
 
 	for _, e := range events {
