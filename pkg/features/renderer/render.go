997e8699daf29437cc7c01788eac5e2eac3c832a
Scott Nichols <n3wscott@tableflip.dev>
2022-08-25 16:28:47 -0700

    working table rendering with AMQP and HTTP with formats
    Signed-off-by: Scott Nichols <n3wscott@tableflip.dev>

--- pkg/features/renderer/render.go
+++ pkg/features/renderer/render.go
@@ -6,110 +6,207 @@
 package renderer
 
 import (
+	"errors"
+	"fmt"
 	"html/template"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/cloudevents/conformance/pkg/features/model"
 )
 
-var table = `<!-- START GENERATED TABLE -->
-<!-- This table was produced by the feature command from https://github.com/cloudevents/conformance -->
-<!-- COMMAND -->
+var table = `
+{{- $true := glyph true }}
+{{- $false := glyph false }}
 <table>
 <thead>
   <tr>
     <th colspan="2">Feature</th>
-	{{- with .Language }}{{- range . }}
+	{{- range .Languages }}
     <th>{{ . }}</th>
-	{{- end }}{{- end }}
+	{{- end }}
   </tr>
 </thead>
 <tbody>
+{{- range .Versions }}
   <tr>
-    <td colspan="2"><strong>v1.0</strong></td>
-    <td></td>
+    <td colspan="2"><strong>{{ .Name }}</strong></td>
+    {{- range .Languages }}
+    <th></th>
+	{{- end }}
   </tr>
   <tr>
-    <td colspan="2"><a href="https://github.com/cloudevents/spec/blob/v1.0/spec.md">CloudEvents Core</a></td>
-    <td> {{ .true }} </td>
+    <td colspan="2"><a href="https://github.com/cloudevents/spec/blob/{{ .Name }}/spec.md">CloudEvents Core</a></td>
+    {{- range .Core }}
+    <td>{{ glyph . }}</td>
+	{{- end }}
   </tr>
+
   <tr>
-    <td colspan="3"></td>
+    <td colspan="{{ fullSpan }}"></td>
   </tr>
-  {{- range $index, $value = .Versions }}
+
+  {{- range .Formats }}
   <tr>
     <td colspan="2">{{ .Name }}</td>
-    <td> {{ .true }} </td>
+    {{- range .Support }}
+    <td>{{ glyph . }}</td>
+	{{- end }}
+  </tr>
+  {{- end }}
+
+  <tr>
+    <td colspan="{{ fullSpan }}"></td>
+  </tr>
+
+  {{- range $bi, $binding := .Bindings }}{{- range $index, $format := $binding.Formats }}
+  <tr>
+    {{- if eq $index 0 }}
+    <td rowspan="{{ len $binding.Formats }}">{{ $binding.Name }}</td>
+    {{- end }}
+    <td>{{ $format.Name }}</td>
+    {{- range $format.Support }}
+    <td>{{ glyph . }}</td>
+	{{- end }}
   </tr>
   {{- end }}{{- end }}
 
-  <tr>
-    <td colspan="2">Avro Event Format</td>
-    <td> {{ .true }} </td>
-  </tr>
-  <tr>
-    <td colspan="3"></td>
-  </tr>
-  <tr>
-    <td rowspan="3">HTTP</td>
-    <td>Binary</td>
-    <td> {{ .true }} </td>
-  </tr>
-  <tr>
-    <td>Structured</td>
-    <td> {{ .true }} </td>
-  </tr>
-  <tr>
-    <td>Batch</td>
-    <td> {{ .false }} </td>
-  </tr>
+{{- end }}
 </tbody>
 </table>
-<!-- END GENERATED TABLE -->
-`
-
-var tableold = `<!-- START GENERATED TABLE -->
-<!-- This table was produced by the feature command from https://github.com/cloudevents/conformance -->
-<!-- COMMAND -->
-| Feature | Foo  |
-| :------ | :-: |
-| **[v1.0](https://github.com/cloudevents/spec/tree/v1.0)** |
-| [CloudEvents Core](https://github.com/cloudevents/spec/blob/v1.0/spec.md) | :heavy_check_mark: |
-| |
-| [Avro Event Format](https://github.com/cloudevents/spec/blob/main/cloudevents/formats/avro-format.md) | :x: |
-| [AMQP Event Format - need link]() | :x: |
-| [JSON Event Format](https://github.com/cloudevents/spec/blob/main/cloudevents/formats/json-format.md) | :heavy_check_mark: |
-| [Protobuf Event Format](https://github.com/cloudevents/spec/blob/main/cloudevents/formats/protobuf-format.md) | :x: |
-||
-| [AMQP Binding](https://github.com/cloudevents/spec/blob/main/cloudevents/bindings/amqp-protocol-binding.md) | :x: |
-| [HTTP Binding](https://github.com/cloudevents/spec/blob/main/cloudevents/bindings/http-protocol-binding.md) | :heavy_check_mark: |
-| [Kafka Binding](https://github.com/cloudevents/spec/blob/main/cloudevents/bindings/kafka-protocol-binding.md) :heavy_check_mark: |
-| [MQTT Binding](https://github.com/cloudevents/spec/blob/main/cloudevents/bindings/mqtt-protocol-binding.md) | :x: |
-| [NATS Binding](https://github.com/cloudevents/spec/blob/main/cloudevents/bindings/nats-protocol-binding.md) | :x: |
-| [WebSockets Binding](https://github.com/cloudevents/spec/blob/main/cloudevents/bindings/websockets-protocol-binding.md) | :x: |
-| |
-| [HTTP Content Mode Binary](https://github.com/cloudevents/spec/blob/v0.3/http-transport-binding.md) | :heavy_check_mark: |
-| [HTTP Content Mode Structured](https://github.com/cloudevents/spec/blob/v0.3/http-transport-binding.md) | :heavy_check_mark: |
-| [HTTP Content Mode Batch](https://github.com/cloudevents/spec/blob/v0.3/http-transport-binding.md) | :heavy_check_mark: |
-| [Kafka Content Mode Structured]() | :heavy_check_mark: |
-| [Kafka Content Mode Binary]() | :heavy_check_mark: |
-| [Kafka Content Mode Structured]() | :heavy_check_mark: |
-| [MQTT Content Mode Binary](https://github.com/cloudevents/spec/blob/v0.3/mqtt-transport-binding.md) | :heavy_check_mark: |
-| [MQTT Content Mode Structured](https://github.com/cloudevents/spec/blob/v0.3/mqtt-transport-binding.md) | :heavy_check_mark: |
-| |
-<!-- END GENERATED TABLE -->
 `
 
 type Renderer struct {
+	Languages []string
+	Versions  []RenderVersion
+}
+
+func (r *Renderer) Version(name string) *RenderVersion {
+	for i, v := range r.Versions {
+		if v.Name == name {
+			return &r.Versions[i]
+		}
+	}
+	// Does not exist.
+	r.Versions = append(r.Versions, RenderVersion{
+		Name:      name,
+		Languages: r.Languages,
+		Core:      make([]bool, len(r.Languages)),
+	})
+	return r.Version(name)
+}
+
+func (r *RenderVersion) Format(name string) *RenderFormat {
+	for i, f := range r.Formats {
+		if f.Name == name {
+			return &r.Formats[i]
+		}
+	}
+	// Does not exist.
+	r.Formats = append(r.Formats, RenderFormat{
+		Name:    name,
+		Support: make([]bool, len(r.Languages)),
+	})
+	return r.Format(name)
+}
+
+func (r *RenderVersion) Binding(name string) *RenderBindings {
+	for i, f := range r.Bindings {
+		if f.Name == name {
+			return &r.Bindings[i]
+		}
+	}
+	// Does not exist.
+	r.Bindings = append(r.Bindings, RenderBindings{
+		Name:      name,
+		Languages: r.Languages,
+	})
+	return r.Binding(name)
+}
+
+type RenderVersion struct {
+	Name      string
+	Languages []string
+	Core      []bool
+	Formats   []RenderFormat
+	Bindings  []RenderBindings
+}
+
+type RenderFormat struct {
+	Name    string
+	Support []bool
+}
+
+type RenderBindings struct {
+	Name      string
+	Languages []string
+	Formats   []RenderBindingFormat
+}
+
+func (r *RenderBindings) Format(name string) *RenderBindingFormat {
+	for i, b := range r.Formats {
+		if b.Name == name {
+			return &r.Formats[i]
+		}
+	}
+	// Does not exist.
+	r.Formats = append(r.Formats, RenderBindingFormat{
+		Name:    name,
+		Support: make([]bool, len(r.Languages)),
+	})
+	return r.Format(name)
+}
+
+type RenderBindingFormat struct {
+	Name    string
+	Support []bool
 }
 
 func (r *Renderer) Render(input []model.Features, out io.Writer) error {
-	tbl, err := template.New("table").Parse(table)
+	if len(input) == 0 {
+		return errors.New("need at least one features config")
+	}
+	tbl, err := template.New("table").Funcs(template.FuncMap{
+		"glyph": func(t bool) string {
+			if t {
+				return ":heavy_check_mark:"
+			}
+			return ":x:"
+		},
+		"fullSpan": func() string {
+			return strconv.Itoa(2 + len(input))
+		},
+	}).Parse(table)
 	if err != nil {
 		return err
 	}
 
-	data := map[string]interface{}{
+	data := Renderer{
+		Languages: make([]string, len(input)),
+	}
+
+	for i, f := range input {
+		data.Languages[i] = f.Language
+		for name, vf := range f.Versions {
+			v := data.Version(name)
+			v.Core[i] = vf.Core
+			v.Format("Avro Event Format").Support[i] = vf.EventFormats.Avro
+			v.Format("AMQP Event Format").Support[i] = vf.EventFormats.AMQP
+			v.Format("JSON Event Format").Support[i] = vf.EventFormats.JSON
+			v.Format("Protobuf Event Format").Support[i] = vf.EventFormats.Protobuf
+
+			v.Binding("AMQP").Format("Binary").Support[i] = vf.Bindings.AMQP.Binary
+			v.Binding("AMQP").Format("Structured").Support[i] = vf.Bindings.AMQP.Structured
+
+			v.Binding("HTTP").Format("Binary").Support[i] = vf.Bindings.HTTP.Binary
+			v.Binding("HTTP").Format("Structured").Support[i] = vf.Bindings.HTTP.Structured
+			v.Binding("HTTP").Format("Batch").Support[i] = vf.Bindings.HTTP.Batch
+		}
+	}
+
+	data1 := map[string]interface{}{
 		"true": ":heavy_check_mark:", "false": ":x:",
 		"Language": []string{"Foo", "Bar", "Baz"},
 		"Versions": []map[string]interface{}{
@@ -123,7 +220,17 @@ func (r *Renderer) Render(input []model.Features, out io.Writer) error {
 			}},
 		},
 	}
+	_ = data1
+
+	_, _ = fmt.Fprintf(out, "<!-- START GENERATED TABLE -->\n")
+	_, _ = fmt.Fprintf(out, "<!-- This table was produced by the feature command from https://github.com/cloudevents/conformance -->\n")
+	_, _ = fmt.Fprintf(out, "<!-- via: %s -->\n", strings.Join(os.Args, " "))
 
 	// TODO: likely have to process input into a flat struct.
-	return tbl.Execute(out, data)
+	if err := tbl.Execute(out, data); err != nil {
+		return err
+	}
+
+	_, _ = fmt.Fprintf(out, "<!-- END GENERATED TABLE -->\n")
+	return nil
 }
